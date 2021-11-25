e15b88df85c64bdc24bf111b534bbfa1958fa725
shu-eiryo <shu.eiryo@airitech.co.jp>
2021-11-25 11:49:30 +0900

    1125

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,21 +1,20 @@
-window.onload=function () {
+window.onload = function () {
     //获取弹窗按钮
-    var btn=document.getElementById("btn");
-    var close=document.getElementsByClassName("close")[0];
-    var myModal=document.getElementById("myModal");
+    var btn = document.getElementById("btn");
+    var close = document.getElementsByClassName("close")[0];
+    var myModal = document.getElementById("myModal");
     //按钮绑定事件
-    btn.onclick=function () {
-     //获取弹窗
-    
-     myModal.style.display="block";
+    btn.onclick = function () {
+        //获取弹窗
+        myModal.style.display = "block";
     }
-    close.onclick=function () {
-     myModal.style.display="none";
+    close.onclick = function () {
+        myModal.style.display = "none";
     }
     //用户点击其他地方关闭弹窗
-    window.onclick=function (event) {
-     if(event.target ==myModal){
-      myModal.style.display="none";
-     }
+    window.onclick = function (event) {
+        if (event.target == myModal) {
+            myModal.style.display = "none";
+        }
     }
-   }
+}
